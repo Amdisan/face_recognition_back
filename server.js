d4032eddf32b1f1341ccb0955ed3c0de1023d746
Amdisan <amdisan1@gmail.com>
2023-01-19 20:04:48 +0200

    done all routes

--- server.js
+++ server.js
@@ -1,94 +1,110 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const corse = require("cors");
+const knex = require("knex");
+
+const pgDB = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1", //localhost
+    user: "postgres",
+    password: "2811Zlata2015",
+    database: "face_recognition_db",
+  },
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(corse());
 
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "Sasha",
-      email: "sasha@gmail.com",
-      password: "banana",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Anna",
-      email: "anna@gmail.com",
-      password: "apples",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
-
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.send("success");
 });
 
 app.post("/signin", (req, res) => {
-  const user = database.users.find(
-    (user) =>
-      user.email === req.body.email && req.body.password === user.password
-    //bcrypt.compareSync(req.body.password, user.password)
-  );
-  if (user) {
-    res.json("success");
-  } else {
-    res.status(400).json("error logging in");
-  }
+  const { email, password } = req.body;
+  pgDB("login")
+    .select("email", "hash")
+    .where("email", "=", email)
+    .then((data) => {
+      if (bcrypt.compareSync(password, data[0].hash)) {
+        return pgDB("users")
+          .select("*")
+          .where("email", "=", email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) =>
+            res.status(400).json(`Unable to get user, ${err.detail}`)
+          );
+      } else {
+        res.status(400).json("Wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json(`Wrong credentials, ${err.detail}`));
+
+  // res.json("success");
+
+  // res.status(400).json("error logging in");
 });
 
 app.post("/register", (req, res) => {
-  const user = database.users.find((user) => user.email === req.body.email);
-  if (user) {
-    res.json("user already exists");
-  } else {
-    const { name, email, password } = req.body;
-    const saltRounds = 10;
-    bcrypt
-      .hash(password, saltRounds)
-      .then((hash) => {
-        database.users.push({
-          id: "125",
-          name: name,
-          email: email,
-          password: hash,
-          entries: 0,
-          joined: new Date(),
+  const { name, email, password } = req.body;
+  const saltRounds = 10;
+  const hash = bcrypt.hashSync(password, saltRounds);
+
+  pgDB
+    .transaction((trx) => {
+      return trx("login")
+        .insert({
+          email,
+          hash,
+        })
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users")
+            .insert({
+              email: loginEmail[0].email,
+              name,
+              joined: new Date(),
+            })
+            .returning("*")
+            .then((user) => {
+              res.json(user[0]);
+            });
         });
-      })
-      .then(() => res.json(database.users.find((user) => user.id === "125")))
-      .catch((error) => res.json(error));
-  }
+    })
+    .catch((err) => {
+      res.status(404).json(`Unable to register, ${err.detail}`);
+    });
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  const user = database.users.find((user) => user.id === id);
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json("no such user");
-  }
+  pgDB("users")
+    .select("*")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(404).json("no such user");
+      }
+    })
+    .catch((err) => res.status(400).json(`error getting user, ${err}`));
 });
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  const user = database.users.find((user) => user.id === id);
-  console.log(user);
-  if (user) {
-    user.entries++;
-    res.json(user.entries);
-  } else {
-    res.status(400).json("not found");
-  }
+  pgDB("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json(`unable to get rank, ${err}`));
 });
 
 app.listen(3001, () => {
